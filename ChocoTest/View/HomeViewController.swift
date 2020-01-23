49ad6a0ab47705efaf0f7eb47edf5099be2ed19e
Maria Eugênia <mariaeugenia.dev@gmail.com>
2020-01-23 01:14:05 -0300

    creating tableView viewCode

--- ChocoTest/View/HomeViewController.swift
+++ ChocoTest/View/HomeViewController.swift
@@ -13,7 +13,16 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let tableView = ProductsTableView()
+        self.view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo:view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo:view.bottomAnchor).isActive = true
+        tableView.setupTableView()
+        
+        
     }
     
 
--- ChocoTest/View/ProductsTableView.swift
+++ ChocoTest/View/ProductsTableView.swift
@@ -9,7 +9,51 @@
 import UIKit
 
 class ProductsTableView: UIView {
-
     
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.alpha = 0
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let tableView = UITableView()
+    
+    init() {
+        super.init(frame: .zero)
+        addSubview(contentView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupTableView() {
+        tableView.separatorColor = .clear
+        tableView.backgroundColor = .clear
+        tableView.layer.backgroundColor = UIColor.clear.cgColor
+        tableView.register(ProductTableViewCell.self, forCellReuseIdentifier: "CELL")
+        contentView.addSubview(tableView)
+        tableView.frame = self.frame
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.reloadData()
+    }
+    
+}
 
+extension ProductsTableView: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath) as? ProductTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
 }
